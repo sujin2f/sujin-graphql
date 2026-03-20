27ee2883c9f49a1e212902f5a145dccbc5794af2
Sujin Choi <sujin.2f@gmail.com>
2026-03-20 16:50:15 -0400

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
 import eslint from '@eslint/js'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
     languageOptions: {
